17f5761ea5dbe5237af0223c88d1a2cafcc131f5
prasannavcs24 <prasannav.cs24@bmsce.ac.in>
2025-12-11 08:58:30 +0530

    Add files via upload

---
+++ threaddemo.java
@@ -0,0 +1,34 @@
+class CollegeThread extends Thread{
+    public void run(){
+        try{
+            while(true){
+                System.out.println("BMS College oif Engineering");
+                Thread.sleep(10000);
+            }
+        }
+    catch(InterruptedException e){
+        System.out.println("Thread Interrupted");
+    }
+    }
+}
+class CSEThread extends Thread{
+    public void run(){
+        try{
+            while(true){
+                System.out.println("CSE");
+                Thread.sleep(2000);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread interrupted");
+        }
+    }
+}
+public class threaddemo{
+    public static void main(String [] s){
+        CollegeThread t1 = new CollegeThread();
+        CSEThread t2= new CSEThread();
+        t1.start();
+        t2.start();
+    }
+}
